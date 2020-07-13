3ed600dfaf423e2562d5c17ef069e7f16b078ec6
liangchengj <liangchengj@outlook.com>
2020-07-14 06:30:23 +0800

    2020/7/14 6:30

--- amuse/src/main/jni/amuse.c
+++ amuse/src/main/jni/amuse.c
@@ -51,18 +51,15 @@ void toast_show(JNIEnv *env, jobject ctx, jobject jobj, jint duration);
 #define WLOG(__TAG__, ...) __android_log_print(ANDROID_LOG_WARN, __TAG__, DEF_LOG_FMT, __VA_ARGS__)
 #define ELOG(__TAG__, ...) __android_log_print(ANDROID_LOG_ERROR, __TAG__, DEF_LOG_FMT, __VA_ARGS__)
 
-inline jclass fdjclz(JNIEnv *env, char const *name)
-{
+inline jclass fdjclz(JNIEnv *env, char const *name) {
     return (*env)->FindClass(env, name);
 }
 
-inline jclass jclazz(JNIEnv *env)
-{
-    return fdjclz("java/lang/Class");
+inline jclass jclazz(JNIEnv *env) {
+    return fdjclz(env, "java/lang/Class");
 }
 
-inline char const *jstr_cstr(JNIEnv *env, jstring jstr)
-{
+inline char const *jstr_cstr(JNIEnv *env, jstring jstr) {
     return (*env)->GetStringUTFChars(env, jstr, JNI_FALSE);
     // char *cs = NULL;
     // /* (*env)->FindClass(env, "java/lang/String") */
@@ -82,118 +79,99 @@ inline char const *jstr_cstr(JNIEnv *env, jstring jstr)
     // return cs;
 }
 
-inline jstring cstr_jstr(JNIEnv *env, char const *cs)
-{
+inline jstring cstr_jstr(JNIEnv *env, char const *cs) {
     return (*env)->NewStringUTF(env, cs);
 }
 
-inline jclass jclz_fromjobj(JNIEnv *env, jobject jobj)
-{
+inline jclass jclz_fromjobj(JNIEnv *env, jobject jobj) {
     return (*env)->GetObjectClass(env, jobj);
 }
 
-inline jobject jclz_callobjmtd(JNIEnv *env, jclass jclz, char const *name, char const *sig, ...)
-{
+inline jobject jclz_callobjmtd(JNIEnv *env, jclass jclz, char const *name, char const *sig, ...) {
     return (*env)->CallObjectMethod(env, jclz,
                                     (*env)->GetMethodID(env, jclazz(env), name,
-                                                        sig),
-                                    ...);
+                                                        sig),va
+    );
 }
 
-inline jobject jclz_callsticmtd(JNIEnv *env, jclass jclz, char const *name, char const *sig, ...)
-{
+inline jobject jclz_callsticmtd(JNIEnv *env, jclass jclz, char const *name, char const *sig, ...) {
     return (*env)->CallStaticObjectMethod(env, jclz,
-                                          (*env)->GetStaticMethodID(env, jclazz(env), name, sig), ...);
+                                          (*env)->GetStaticMethodID(env, jclazz(env), name, sig),
+                                          ...);
 }
 
-inline jobject jclz_callctor(JNIEnv *env, jclass jclz, char const *sig, ...)
-{
+inline jobject jclz_callctor(JNIEnv *env, jclass jclz, char const *sig, ...) {
     return jclz_callobjmtd(env, jclz, "<init>", sig, ...);
 }
 
-inline jobject jobj_callobjmtd(JNIEnv *env, jobject jobj, char const *name, char const *sig, ...)
-{
+inline jobject jobj_callobjmtd(JNIEnv *env, jobject jobj, char const *name, char const *sig, ...) {
     return (*env)->CallObjectMethod(env, jobj,
                                     (*env)->GetMethodID(env, jclz_fromjobj(env, jobj), name,
-                                                        sig),...);
+                                                        sig), ...);
 }
 
-inline jstring jclz_name(JNIEnv *env, jclass jclz)
-{
-    return (jstring)jclz_callobjmtd(env, jclz, "getName", "()Ljava/lang/String;");
+inline jstring jclz_name(JNIEnv *env, jclass jclz) {
+    return (jstring) jclz_callobjmtd(env, jclz, "getName", "()Ljava/lang/String;");
 }
 
-inline jstring jclz_simp_name(JNIEnv *env, jclass jclz)
-{
-    return (jstring)jclz_callobjmtd(env, jclz, "getSimpleName", "()Ljava/lang/String;");
+inline jstring jclz_simp_name(JNIEnv *env, jclass jclz) {
+    return (jstring) jclz_callobjmtd(env, jclz, "getSimpleName", "()Ljava/lang/String;");
 }
 
-inline jstring jobj_tostr(JNIEnv *env, jstring jstr)
-{
-    return (jstring)jobj_callobjmtd(env, jstr, "toString", "()Ljava/lang/String;");
+inline jstring jobj_tostr(JNIEnv *env, jstring jstr) {
+    return (jstring) jobj_callobjmtd(env, jstr, "toString", "()Ljava/lang/String;");
 }
 
-inline jobject jbyte_(JNIEnv *env, jbyte jbt)
-{
-    return jclz_callctor(env, fdjclz("java/lang/Byte"), "(B)V", jbt);
+inline jobject jbyte_(JNIEnv *env, jbyte jbt) {
+    return jclz_callctor(env, fdjclz(env, "java/lang/Byte"), "(B)V", jbt);
 }
 
-inline jobject jshort_(JNIEnv *env, jshort jsh)
-{
-    return jclz_callctor(env, fdjclz("java/lang/Short"), "(S)V", jsh);
+inline jobject jshort_(JNIEnv *env, jshort jsh) {
+    return jclz_callctor(env, fdjclz(env, "java/lang/Short"), "(S)V", jsh);
 }
 
-inline jobject jint_(JNIEnv *env, jint ji)
-{
-    return jclz_callctor(env, fdjclz("java/lang/Integer"), "(I)V", ji);
+inline jobject jint_(JNIEnv *env, jint ji) {
+    return jclz_callctor(env, fdjclz(env, "java/lang/Integer"), "(I)V", ji);
 }
 
-inline jobject jlong_(JNIEnv *env, jlong jl)
-{
-    return jclz_callctor(env, fdjclz("java/lang/Long"), "(J)V", jl);
+inline jobject jlong_(JNIEnv *env, jlong jl) {
+    return jclz_callctor(env, fdjclz(env, "java/lang/Long"), "(J)V", jl);
 }
 
-inline jobject jfloat_(JNIEnv *env, jfloat jf)
-{
-    return jclz_callctor(env, fdjclz("java/lang/Float"), "(F)V", jf);
+inline jobject jfloat_(JNIEnv *env, jfloat jf) {
+    return jclz_callctor(env, fdjclz(env, "java/lang/Float"), "(F)V", jf);
 }
 
-inline jobject jdouble_(JNIEnv *env, jdouble jd)
-{
-    return jclz_callctor(env,fdjclz("java/lang/Double"）,"(D)V",jd);
+inline jobject jdouble_(JNIEnv *env, jdouble jd) {
+    return jclz_callctor(env, fdjclz(env, "java/lang/Double"), "(D)V", jd);
 }
 
-inline jobject jboolean_(JNIEnv *env, jboolean jbool)
-{
-    return jclz_callctor(env, fdjclz("java/lang/Boolean"), "(Z)V", jbool);
+inline jobject jboolean_(JNIEnv *env, jboolean jbool) {
+    return jclz_callctor(env, fdjclz(env, "java/lang/Boolean"), "(Z)V", jbool);
 }
 
-inline jobject jchar_(JNIEnv *env, jchar jch)
-{
-    return jclz_callctor(env, fdjclz("java/lang/Character"), "(C)V", jch);
+inline jobject jchar_(JNIEnv *env, jchar jch) {
+    return jclz_callctor(env, fdjclz(env, "java/lang/Character"), "(C)V", jch);
 }
 
-inline jobject boxing(JNIEnv *env, char const *wrapper_name, ...)
-{
+inline jobject boxing(JNIEnv *env, char const *wrapper_name, ...) {
     char const *clzn_begin = "java/lang/";
-    char *clzn = (char *)malloc(strlen(clzn_begin) + strlen(wrapper_name));
-    strcpy(clzn,clzn_begin);
-    strcat(clzn,wrapper_name);
-    return jclz_callsticmtd(env, fdjclz(clzn),"valueOf");
+    char *clzn = (char *) malloc(strlen(clzn_begin) + strlen(wrapper_name));
+    strcpy(clzn, clzn_begin);
+    strcat(clzn, wrapper_name);
+    return jclz_callsticmtd(env, fdjclz(env, clzn), "valueOf");
 }
 
-inline jint jint_unboxing(JNIEnv *env, jobject jobj)
-{
+inline jint jint_unboxing(JNIEnv *env, jobject jobj) {
 }
 
-inline void toast_show(JNIEnv *env, jobject ctx, jobject jobj, jint duration)
-{
+inline void toast_show(JNIEnv *env, jobject ctx, jobject jobj, jint duration) {
     char *param_sign = jclz_name(env, jclz_fromjobj(env, jobj)) == "java.lang.Integer" ? "I"
                                                                                        : "Ljava/lang/CharSequence;";
 
     char const *sign_begin = "(Landroid/content/Context;";
     char const *sign_end = "I)Landroid/widget/Toast;";
-    char *sign = (char *)malloc(strlen(sign_begin) + strlen(param_sign) + strlen(sign_end));
+    char *sign = (char *) malloc(strlen(sign_begin) + strlen(param_sign) + strlen(sign_end));
     strcpy(sign, sign_begin);
     strcat(sign, param_sign);
     strcat(sign, sign_end);
@@ -202,7 +180,8 @@ inline void toast_show(JNIEnv *env, jobject ctx, jobject jobj, jint duration)
     jmethodID jmid_makeText = (*env)->GetStaticMethodID(env, jclz_Toast, "makeText", sign);
     jobject jobj_Toast = (*env)->CallStaticObjectMethod(env, jclz_Toast, jmid_makeText,
                                                         ctx,
-                                                        param_sign == "I" ? (jint)jobj : jobj_tostr(env, jobj),
+                                                        param_sign == "I" ? (jint) jobj
+                                                                          : jobj_tostr(env, jobj),
                                                         duration);
     jmethodID jmid_show = (*env)->GetMethodID(env, jclz_Toast, "show", "()V");
     (*env)->CallVoidMethod(env, jobj_Toast, jmid_show);
@@ -210,64 +189,52 @@ inline void toast_show(JNIEnv *env, jobject ctx, jobject jobj, jint duration)
 
 JNIEXPORT void JNICALL
 Java_com_meyoustu_amuse_App_verboseLog(JNIEnv *env, jobject thiz, jboolean if_debug, jstring tag,
-                                       jobject msg)
-{
-    if (if_debug == JNI_TRUE)
-    {
+                                       jobject msg) {
+    if (if_debug == JNI_TRUE) {
         VLOG(jstr_cstr(env, tag), jstr_cstr(env, jobj_tostr(env, msg)));
     }
 }
 
 JNIEXPORT void JNICALL
 Java_com_meyoustu_amuse_App_debugLog(JNIEnv *env, jobject thiz, jboolean if_debug, jstring tag,
-                                     jobject msg)
-{
-    if (if_debug == JNI_TRUE)
-    {
+                                     jobject msg) {
+    if (if_debug == JNI_TRUE) {
         DLOG(jstr_cstr(env, tag), jstr_cstr(env, jobj_tostr(env, msg)));
     }
 }
 
 JNIEXPORT void JNICALL
 Java_com_meyoustu_amuse_App_infoLog(JNIEnv *env, jobject thiz, jboolean if_debug, jstring tag,
-                                    jobject msg)
-{
-    if (if_debug == JNI_TRUE)
-    {
+                                    jobject msg) {
+    if (if_debug == JNI_TRUE) {
         ILOG(jstr_cstr(env, tag), jstr_cstr(env, jobj_tostr(env, msg)));
     }
 }
 
 JNIEXPORT void JNICALL
 Java_com_meyoustu_amuse_App_warnLog(JNIEnv *env, jobject thiz, jboolean if_debug, jstring tag,
-                                    jobject msg)
-{
-    if (if_debug == JNI_TRUE)
-    {
+                                    jobject msg) {
+    if (if_debug == JNI_TRUE) {
         WLOG(jstr_cstr(env, tag), jstr_cstr(env, jobj_tostr(env, msg)));
     }
 }
 
 JNIEXPORT void JNICALL
 Java_com_meyoustu_amuse_App_errorLog(JNIEnv *env, jobject thiz, jboolean if_debug, jstring tag,
-                                     jobject msg)
-{
-    if (if_debug == JNI_TRUE)
-    {
+                                     jobject msg) {
+    if (if_debug == JNI_TRUE) {
         ELOG(jstr_cstr(env, tag), jstr_cstr(env, jobj_tostr(env, msg)));
     }
 }
 
 JNIEXPORT void JNICALL
 Java_com_meyoustu_amuse_util_Toast_toastMsg(JNIEnv *env, jobject thiz,
-                                            jobject ctx, jobject msg, jint duration)
-{
+                                            jobject ctx, jobject msg, jint duration) {
     toast_show(env, ctx, msg, duration);
 }
 
 JNIEXPORT void JNICALL
 Java_com_meyoustu_amuse_util_Toast_toastRes(JNIEnv *env, jobject thiz,
-                                            jobject ctx, jint id, jint duration)
-{
+                                            jobject ctx, jint id, jint duration) {
     toast_show(env, ctx, id, duration);
 }
